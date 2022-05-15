2fc33cd41fad86f83ac6b6b0070f1693ce3af127
Michael54677 <michaeljonathan546@gmail.com>
2022-05-15 04:00:14 -0700

    updated file

---
+++ conditional_test_with_if_statement.py
@@ -0,0 +1,17 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+password = 'michael'
+password == 'michael'
+True
+password == 'Michael'
+False
+password == 'michael'
+True
+password = 'Michael'
+password == 'Michael'
+True
+password == 'michael'
+False
+passowrd1 = 'Michael'
+password.lower() == 'michael'
+True
---
+++ if_ststment.py
@@ -0,0 +1,12 @@
+### 10 / 05 / 2022
+### Author: Michael Jonathan
+### if ststment
+
+print("To continue please log in.")
+
+#Using the if statement
+password = input("Please enter your password.")
+if password == "michael":
+    print("Access granted.")
+else:
+    print("Incorrect. Please try again.")
