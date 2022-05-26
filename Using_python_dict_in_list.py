6539f7268a1a95e43a6fe4686997c8e02fe0c9dc
Michael54677 <michaeljonathan546@gmail.com>
2022-05-25 19:38:01 -0700

    Updated file

---
+++ Using_python_dict_in_list.py
@@ -0,0 +1,12 @@
+### 21 / 05 / 2022
+### Author: Michael Jonathan
+### Using a dictionary in a list
+
+book_0 = {'name' : 'elon musk', 'author' : 'ashlee vance', 'price' : '14.99', 'publisher' : 'virgin books'}
+book_1 = {'name' : 'team of rivals', 'author' : 'doris kearns goodwin', 'price' : '10.99', 'publisher' : 'pengiun'}
+book_2 = {'name' : 'the everything store', 'author' : 'brad stone', 'price' : '12.99', 'publisher' : 'bantam press'}
+
+books = [book_0, book_1, book_2]
+
+for book in books:
+    print(book)
