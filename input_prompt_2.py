a582ff2486a4b9be66ff3d80db708e09aec24142
Michael54677 <michaeljonathan546@gmail.com>
2022-05-27 14:59:15 -0700

    Updated file

---
+++ input_prompt_2.py
@@ -0,0 +1,7 @@
+### 24 / 05 / 2022
+### Author: Michael Jonathan
+### Using the input prompt
+
+prompt = "This is our lecture discussing python's input prompt and how to keep it tidy. Please enter your favorite lecture so far and why you like it?"
+
+favorite_lecture = input(prompt)
