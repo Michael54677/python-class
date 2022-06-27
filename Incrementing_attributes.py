19afec27c2481272be7f7ef034f68d7de142d387
Michael54677 <michaeljonathan546@gmail.com>
2022-06-27 11:06:03 -0700

    updated file

--- Incrementing_attributes.py
+++ Incrementing_attributes.py
@@ -1,7 +1,6 @@
-### 25 / 06 / 2022
+### 27 / 06 / 2022
 ### Author: Michael Jonathan
-### Creating an ereader class
-
+### Incrementing an attributes
 
 class Ereader():
     """A class to repesent an ereader."""
@@ -14,11 +13,30 @@ class Ereader():
         self.backlight = backlight
         self.battery = battery
         self.screen_type = screen_type
+        self.library_count = 0
 
     def get_ereader_name(self):
         """Return a formatted descriptive name for the ereader."""
         long_name = self.make + ' - ' + self.model + ' - ' + self.backlight + ' - ' + self.battery + ' - ' + self.screen_type
         return long_name.title()
 
+    def read_library_count(self):
+        """show the amount of ebooks in the kindle library."""
+        print("You have " + str(self.library_count) + " books in your kindle libraby. ")    
+
+    def update_library_count(self, ebook_count):
+        """set the library count."""
+        self.library_count = ebook_count
+
+    def increment_library_count(self, purchased_ebooks):
+        """Add the new ebooks to the library count."""
+        self.library_count += purchased_ebooks   
+
 new_ereader = Ereader('Amazon Kindle', 'Paperwhite', 'Adjustable Backlight', 'Several Months of Battery Life', '300 Dpi') 
-print(new_ereader.get_ereader_name())        
+print(new_ereader.get_ereader_name())
+
+new_ereader.update_library_count(65)
+new_ereader.read_library_count
+
+new_ereader.increment_library_count(15)
+new_ereader.read_library_count()
--- Modifying_attributes_value_directly.py
+++ Modifying_attributes_value_directly.py
@@ -1,7 +1,6 @@
-### 25 / 06 / 2022
+### 27 / 06 / 2022
 ### Author: Michael Jonathan
-### Creating an ereader class
-
+### Modifying an attributes value directly
 
 class Ereader():
     """A class to repesent an ereader."""
@@ -14,11 +13,19 @@ class Ereader():
         self.backlight = backlight
         self.battery = battery
         self.screen_type = screen_type
+        self.library_count = 0
 
     def get_ereader_name(self):
         """Return a formatted descriptive name for the ereader."""
         long_name = self.make + ' - ' + self.model + ' - ' + self.backlight + ' - ' + self.battery + ' - ' + self.screen_type
         return long_name.title()
 
+    def read_library_count(self):
+        """show the amount of ebooks in the kindle library."""
+        print("You have " + str(self.library_count) + " books in your kindle libraby. ")    
+
 new_ereader = Ereader('Amazon Kindle', 'Paperwhite', 'Adjustable Backlight', 'Several Months of Battery Life', '300 Dpi') 
-print(new_ereader.get_ereader_name())        
+print(new_ereader.get_ereader_name())
+
+new_ereader.library_count = 36
+new_ereader.read_library_count()
